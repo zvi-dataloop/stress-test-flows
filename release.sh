88ea102341c70212841220a1654be22373aa0320
Zvi Lorber <zvi@dataloop.ai>
2026-02-04 14:48:24 +0200

    1.0.42-podtype-m-3

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a 1.0.42-podtype-m-2 -m "Release version 1.0.42-podtype-m-2"
-git push --force origin 1.0.42-podtype-m-2
+git tag -a 1.0.42-podtype-m-3 -m "Release version 1.0.42-podtype-m-3"
+git push --force origin 1.0.42-podtype-m-3
