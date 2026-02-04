23f2e23162bbcdd41f5ad5148c6ed5821a2dcd6f
Zvi Lorber <zvi@dataloop.ai>
2026-02-04 11:50:52 +0200

    56

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a 1.0.55 -m "Release version 1.0.55"
-git push --force origin 1.0.55              
+git tag -a 1.0.56 -m "Release version 1.0.56"
+git push --force origin 1.0.56              
