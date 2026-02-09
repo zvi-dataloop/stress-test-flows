f504cb6fcbe2c60bbe84b99b145505e908339917
Zvi Lorber <zvi@dataloop.ai>
2026-02-09 13:42:38 +0200

    metrics3

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a 1.0.67-metrics2 -m "Release version 1.0.67-metrics2"
-git push --force origin 1.0.67-metrics2              
+git tag -a 1.0.67-metrics3 -m "Release version 1.0.67-metrics3"
+git push --force origin 1.0.67-metrics3                 
