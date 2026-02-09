15aba793fd2586c168ab89ce92aa8c32824855d6
Zvi Lorber <zvi@dataloop.ai>
2026-02-09 16:07:55 +0200

    7

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a 1.0.67-metrics6 -m "Release version 1.0.67-metrics6"
-git push --force origin 1.0.67-metrics6
+git tag -a 1.0.67-metrics7 -m "Release version 1.0.67-metrics7"
+git push --force origin 1.0.67-metrics7
