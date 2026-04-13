f8d3667a5f43386da0f9b0f74a7f09051006841a
Zvi Lorber <zvi@dataloop.ai>
2026-04-13 17:24:27 +0300

    1.0.86-streamer-5

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a 1.0.86-streamer-4 -m "Release version 1.0.86-streamer-4"
-git push --force origin 1.0.86-streamer-4
+git tag -a 1.0.86-streamer-5 -m "Release version 1.0.86-streamer-5"
+git push --force origin 1.0.86-streamer-5
